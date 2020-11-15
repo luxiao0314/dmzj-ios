7188ed43b54fb43290f19552d63579914b9ce2c5
xiao.lu <luxiao@qutoutiao.net>
2020-11-15 12:23:26 +0800

    Initial Commit

---
+++ dmzj/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dmzj
+//
+//  Created by lucio on 2020/11/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dmzj/dmzjApp.swift
@@ -0,0 +1,17 @@
+//
+//  dmzjApp.swift
+//  dmzj
+//
+//  Created by lucio on 2020/11/15.
+//
+
+import SwiftUI
+
+@main
+struct dmzjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
